1eb6bdb2f04a80a23bf236ecb6733cd370107743
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2024-12-18 19:22:27 +0100

    feat: Solved 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function () {
+    const argumentsArray = Array.from(arguments);
+    const inputArray = argumentsArray[0];
+    argumentsArray.shift();
+    const elementsToRemove = argumentsArray;
 
+    elementsToRemove.forEach((element) => {
+        while (inputArray.includes(element)) {
+            const indexOfElement = inputArray.findIndex((value) => value === element);
+            inputArray.splice(indexOfElement, 1);
+        }
+    });
+    return inputArray;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
