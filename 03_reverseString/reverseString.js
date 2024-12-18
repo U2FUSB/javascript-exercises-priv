9de82d7a1f645ba812f24792e3364593339a8b88
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2024-12-18 17:42:58 +0100

    feat: Solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (inputString) {
+    const inputCharArray = inputString.split("");
+    const outputCharArray = [];
+    for (inputChar of inputCharArray) {
+        outputCharArray.unshift(inputChar);
+    }
+    return outputCharArray.join("");
 };
 
 // Do not edit below this line
