e8bec16150362a87ba2cddff0f0b1ca8cd7e68ce
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2024-12-18 17:13:06 +0100

    feat: Solved 01-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (inputString, repeatTimes) {
+    let outputString = "";
+    if (repeatTimes < 0) return "ERROR";
+    for (let index = 1; index <= repeatTimes; index++) {
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
