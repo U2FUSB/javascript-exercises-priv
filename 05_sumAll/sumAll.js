cc7226fcba96eefd413b6d41c0d5faa66bebfc1d
LetsBuildAFlyingDonkey <48519981+U2FUSB@users.noreply.github.com>
2024-12-18 20:39:27 +0100

    feat: Solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+    if (numStart > numEnd) {
+        const buffer = numStart;
+        numStart = numEnd;
+        numEnd = buffer;
+    }
+    if (
+        !(
+            Number.isInteger(numStart) &&
+            numStart > 0 &&
+            Number.isInteger(numEnd) &&
+            numEnd > 0
+        )
+    )
+        return "ERROR";
+    const sum = function sumAllRecursion(_numStart, _numEnd) {
+        if (_numStart === _numEnd) return _numEnd;
+        return _numStart + sumAllRecursion(_numStart + 1, _numEnd);
+    };
+    return sum(numStart, numEnd);
 };
 
 // Do not edit below this line
